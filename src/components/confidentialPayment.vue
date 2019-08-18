f89eb26f5d45d1b830bdc87832a33ae55db49d8c
MeiYan-81 <53925195+MeiYan-81@users.noreply.github.com>
2019-08-18 20:51:00 +0800

    Add files via upload

---
+++ src/components/confidentialPayment.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <van-nav-bar left-arrow left-text="返回" @click-left="onClickLeft" />
+
+    <img style="display: block; margin: 0 auto;" src="../../static/exempt.png" alt="">
+    <div style="text-align: center;margin: 20px 0;">
+      您好, {{ username || '请完善个人资料' }}
+    </div>
+
+    <van-cell-group>
+      <van-field label="请输入密码" type="password" v-model="password" placeholder="请输入密码" />
+    </van-cell-group>
+
+    <van-button type="info" style="display: block; margin: 50px auto; width: 200px; border-radius: 6px;" @click="submit">确认</van-button>
+  </div>
+</template>
+
+<script>
+	//需要导入vuex的数据管理
+	import store from '@/vuex/store'
+  export default {
+    methods: {
+      onClickLeft() {
+        this.$router.go(-1);
+      },
+      submit() {
+        if(this.password.length==0){
+          this.$notify("请确认输入信息");
+          return;
+        }
+		
+		this.$http.post("/confidential-payment",{
+			userId:store.state.id,
+			password: this.password,
+		})
+		.then(res =>{
+			console.log(res);
+			debugger
+			if(res.error_code == 200){
+				this.$notify(res.meg);
+				
+				this.$router.push({path:'/my',query:{}});
+				
+			}else{
+				this.$notify(res.meg);
+			}
+		});
+      }
+    },
+    data() {
+      return {
+        username: '',
+        password: '',
+      }
+    },
+  }
+</script>
+
+<style>
+</style>
---
+++ src/components/returnTheDepositMoney.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <van-nav-bar left-arrow left-text="返回" @click-left="onClickLeft" />
+
+    <img style="display: block; margin: 0 auto;" src="../../static/detain.png" alt="">
+    <div style="text-align: center;margin: 20px 0;">
+      押金： {{ money || 0.00 }}￥
+    </div>
+
+    <van-button type="info" style="display: block; margin: 50px auto; width: 200px; border-radius: 6px;" @click="refund">申请退还押金</van-button>
+	<van-button type="primary" style="display: block; margin: -17px auto; width: 200px; border-radius: 5px;" @click="handIn">交押金</van-button>
+  </div>
+</template>
+
+<script>
+	//需要导入vuex的数据管理
+	import store from '@/vuex/store'
+  export default {
+	  
+	mounted(){
+		
+		var _this = this;
+		
+		this.$http.get("/inquiryAmount?userId="+store.state.id)
+		.then(res =>{
+			console.log(res);
+			debugger
+			if(res.error_code == 200){
+				this.$notify(res.meg);
+				_this.money = res.data.money;
+			}else{
+				this.$notify(res.meg);
+			}
+		});
+		
+	},
+	  
+    methods: {
+      onClickLeft() {
+        this.$router.go(-1);
+      },
+      handIn() {
+        this.$http.post("/payDeposit?userId="+store.state.id)
+        .then(res =>{
+        	console.log(res);
+        	debugger
+        	if(res.error_code == 200){
+        		this.$notify(res.meg);
+        		
+        	}else{
+        		this.$notify(res.meg);
+        	}
+					
+					this.$router.push({path:'/my',query:{}});
+        });
+      },
+	  refund() {
+	   this.$http.post("/deposit-refund?userId="+store.state.id)
+	   .then(res =>{
+	   	console.log(res);
+	   	debugger
+	   	if(res.error_code == 200){
+	   		this.$notify(res.meg);
+	   		
+	   	}else{
+	   		this.$notify(res.meg);
+	   	}
+			this.$router.push({path:'/my',query:{}});
+	   });
+	  }
+    },
+    data() {
+      return {
+        money: '',
+      }
+    },
+  }
+</script>
+
+<style>
+</style>
